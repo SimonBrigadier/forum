840d47f84fd1ee62bbcd9c652b41351bb65bef78
SimonBrigadier <brigadenko.s.p@gmail.com>
2017-09-05 12:05:39 +0300

    forum ido v.3.2
    Добавил файлы

---
+++ lang/ru/forum.php
@@ -0,0 +1,23 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Strings for component 'forum', language 'ru', branch 'MOODLE_20_STABLE'
+ *
+ * @package   mod_forum
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['numofstudents'] = 'Студентов без оценки';
+$string['israted'] = 'Оценка';
+$string['modforumpstgu'] = 'Форум для контрольной работы';
+$string['modforumpstgunotify'] = 'Это форум для контрольной работы. Для того, чтобы увидеть ответы других участников на поставленный вопрос, Вам необходимо получить оценку за свой ответ';
+$string['completiongradeeachdiscussion'] = 'Оценка за каждую тему.';
+$string['completiongradeeachdiscussion_help'] = 'Для завершения этого элемента студенту необходимо получить оценку за каждую тему.';
+
